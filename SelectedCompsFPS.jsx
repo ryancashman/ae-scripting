aaa06ff3e25e317a81c168cb82c605fd2bf91fa3
Ryan Cashman <ryan@nanoanimal.com>
2018-04-11 17:31:58 -0700

    selected comps fps script

---
+++ SelectedCompsFPS.jsx
@@ -0,0 +1,13 @@
+﻿#target aftereffects
+app.beginUndoGroup("Selected Comps FPS");
+
+//var newDuration = prompt ("Enter New Duration", 10, "title");
+var fps = prompt("Enter new frame rate", 24);
+for (i = 1; i < app.project.items.length+1; i++)
+{
+    curItem = app.project.items[i];
+    if (curItem instanceof CompItem && curItem.selected)
+    {
+      curItem.frameRate = fps;
+        }
+    }
